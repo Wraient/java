72efd014e6805c1549156a658c74a10003de2ac0
Wraient Daemon <81632222+wraient@users.noreply.github.com>
2021-07-12 18:40:22 +0530

    Create WhileDoWhile.java

---
+++ Introduction/src/sevenJuneTwentyOne/WhileDoWhile.java
@@ -0,0 +1,43 @@
+package sevenJuneTwentyOne;
+
+// import java.util.Scanner;
+
+public class WhileDoWhile {
+    public static void main(String[] args) {
+        // Scanner sc  = new Scanner(System.in);
+        // int num = sc.nextInt();
+        /*
+        int temp = num;
+        int sum = 0;
+        while(temp>0){
+            int lastDigit = temp % 10;
+            temp /= 10;
+            sum += lastDigit;
+        }
+        System.out.println(sum);
+        */
+        
+        // Pallindrome checker
+        /* 
+        int temp = num, reverse = 0;
+        while(temp>0){
+            int lastDigit = temp % 10;
+            reverse = reverse*10+lastDigit;
+            temp /= 10;
+        }
+        if (reverse == num){
+            System.out.println("This is a pallindrome number.");
+        } else{
+            System.out.println("This isn't a pallindrome");
+        }
+        */
+
+        // do while
+        /*
+        do{
+            System.out.println("Hello World");
+        } while(num<10);
+        */
+    }
+    
+}
