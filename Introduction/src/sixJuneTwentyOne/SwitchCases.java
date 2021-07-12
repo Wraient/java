220de6e932dbe0a5cb6a3c02fc1852333f7e8feb
Wraient Daemon <81632222+wraient@users.noreply.github.com>
2021-07-12 18:45:45 +0530

    Create SwitchCases.java

---
+++ Introduction/src/sixJuneTwentyOne/SwitchCases.java
@@ -0,0 +1,66 @@
+package sixJuneTwentyOne;
+// import java.util.Scanner;
+
+public class SwitchCases {
+    public static void main(String[] args) {
+        /*
+        int dayOfWeek = 69;
+        switch(dayOfWeek){
+            case 1:
+                System.out.println("Today is Sunday");
+                break;
+            case 2:
+                System.out.println("Today is Monday");
+                break;
+            case 3:
+                System.out.println("Today is Tuesday");
+                break;
+            case 4:
+                System.out.println("Today is Wenesday");
+                break;
+            case 5:
+                System.out.println("Today is Thursday");
+                break;
+            case 6:
+                System.out.println("Today is Friday");
+                break;
+            case 7:
+                System.out.println("Today is Saturday");
+                break;
+            default:
+                System.out.println("This isn't a day");
+            break;
+        }
+        */
+
+
+        /*
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Eneter First Number : ");
+        int fNum = sc.nextInt();
+        System.out.println("Eneter Second Number : ");
+        int sNum = sc.nextInt();
+        System.out.println("Eneter operation(+, -, /, *) : ");
+        sc.nextLine();
+        char operation = sc.nextLine().charAt(0);
+
+        switch(operation){
+            case '+':
+                System.out.println(fNum+sNum);
+                break;
+            case '-':
+                System.out.println(fNum-sNum);
+                break;
+            case '*':
+                System.out.println(fNum*sNum);
+                break;
+            case '/':
+                System.out.println(fNum/sNum);
+                break;
+            default:
+                System.out.println("Invalid operation");
+        }
+        */
+    }
+    
+}
