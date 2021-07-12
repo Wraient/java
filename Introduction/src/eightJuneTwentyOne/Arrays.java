500c4bef6d61a6d38b6d751e33cb27e9ebfb9259
Wraient Daemon <81632222+wraient@users.noreply.github.com>
2021-07-12 18:31:25 +0530

    Create Arrays.java

---
+++ Introduction/src/eightJuneTwentyOne/Arrays.java
@@ -0,0 +1,46 @@
+package eightJuneTwentyOne;
+
+// import java.util.Scanner;
+
+public class Arrays {
+    public static void main(String[] args) {
+        // Scanner sc = new Scanner(System.in);
+
+        // Declaring arrays
+
+        // int[] marks = new int[5];
+        
+        // int[] marks;
+        // marks = new int[5];
+
+        // int marks[] = new int[5];
+        // int marks1[] = {34,35,32,69,96};
+
+        // int marks[], calsses; // only marks is array and not classes
+        // int marks[], calsses[]; // both marks and classes are array
+        
+        // int[] marks, classes; // both marks and classes are array
+        // marks[4] = 69;
+        // System.out.println(marks[4]);
+        // System.out.println(marks1);
+
+        /* 
+        System.out.print("Enter the number of students : ");
+        int numOfStudents = sc.nextInt();
+        int students[] = new int[numOfStudents];
+        for(int i=0; i<students.length; i++){
+            System.out.println("Enter the marks of student " + (i+1));
+            students[i] = sc.nextInt();
+        }
+        for(int i=0; i<students.length; i++){
+            System.out.println("Student "+ (i+1) +" has " +students[i]+" Marks.");
+        }
+        int sum = 0;
+        for(int i=0; i<students.length; i++){
+            sum += students[i];
+        }
+        System.out.println("Average of all class is " + (sum/students.length));
+         */
+    }
+    
+}
