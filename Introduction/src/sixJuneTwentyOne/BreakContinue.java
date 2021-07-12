e31b73223823d9555542fa6017ef08bfef995cd0
Wraient Daemon <81632222+wraient@users.noreply.github.com>
2021-07-12 18:41:00 +0530

    Create BreakContinue.java

---
+++ Introduction/src/sixJuneTwentyOne/BreakContinue.java
@@ -0,0 +1,45 @@
+package sixJuneTwentyOne;
+// import java.util.Scanner;
+public class BreakContinue{
+    public static void main(String[] args){
+        // Scanner sc = new Scanner(System.in);
+        /* 
+        for(int i=1; i<=100; i++){
+            System.out.println(i);
+            if(i==69){
+                break;
+            }
+        }
+         */
+        
+         /* for(;;){
+            System.out.println("Enter a number : ");
+            int input = sc.nextInt();
+            if(input==69 || input==-69){
+                System.out.println("You Smart!");
+                break;
+            }
+        }
+        */
+
+        /*
+        for(int i=1; i<=100; i++){
+            if(i==69){
+                continue;
+            }
+            System.out.println(i);
+        }
+        */
+        
+        
+        /* 
+        for(int i=1; i<=10; i++){
+            for(int j=1; j<=20; j++){
+                System.out.print(j*i + "  ");
+            }
+            System.out.println();
+        }
+         */
+
+    }
+}
