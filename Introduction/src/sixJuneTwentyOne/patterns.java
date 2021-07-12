68d0dcc55b1eb31150d4f7a8bc63268be15d0db0
Wraient Daemon <81632222+wraient@users.noreply.github.com>
2021-07-12 18:45:17 +0530

    Create patterns.java

---
+++ Introduction/src/sixJuneTwentyOne/patterns.java
@@ -0,0 +1,57 @@
+package sixJuneTwentyOne;
+// import java.util.Scanner;
+
+public class patterns {
+    public static void main(String[] args){
+        // Scanner sc = new Scanner(System.in);
+        // int num = sc.nextInt();
+        /* 
+        for(int i=1; i<num; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for (int i=1; i<=num; i++){
+            for(int j=1; j<=num; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for(int i=0; i<=num; i++){
+            for(int j=num-i; j>0; j-=1){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        */
+
+        /* 
+        for(int i=1; i<=num; i++){
+            for(int j=1; j<=num; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+        for(int i=1; i<=num; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+        for(int i=1; i<=num; i++){
+            for(int j=num+1-i; j>0; j--){
+                System.out.print("* ");
+            }
+            System.out.println();
+            for(int k=0; k<=i-1; k++){
+                System.out.print("  ");
+            }
+        }
+        */
+    }
+}
