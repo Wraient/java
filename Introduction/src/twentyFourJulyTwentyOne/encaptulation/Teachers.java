270c68684c054ccee8dd2a039857cb2e1fcc8c3d
Wraient Daemon <81632222+wraient@users.noreply.github.com>
2021-07-25 12:21:33 +0530

    Update and rename Introduction/src/twentyFourJulyTwentyOne/encapsulationP2/encaptulation.java to Introduction/src/twentyFourJulyTwentyOne/encaptulation/Teachers.java

---
+++ Introduction/src/twentyFourJulyTwentyOne/encaptulation/Teachers.java
@@ -0,0 +1,13 @@
+package twentyFourJulyTwentyOne.encaptulation;
+
+public class Teachers {
+    public static void main(String[] args){
+        Student obj = new Student("Ajay");
+        System.out.println(obj.getname());
+        System.out.println(obj.WeakestPass());
+        System.out.println(obj.MediumPass());
+        // System.out.println(obj.StrongPass()); // Not Visible
+
+    }
+
+}
