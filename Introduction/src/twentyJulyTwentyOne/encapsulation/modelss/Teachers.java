fb34a2602f1bfbb96ca3a9989b44e5bf0d992f42
Wraient Daemon <81632222+wraient@users.noreply.github.com>
2021-07-25 12:50:17 +0530

    Delete Introduction/src/twentyJulyTwentyOne directory

--- Introduction/src/twentyJulyTwentyOne/encapsulation/modelss/Teachers.java
+++
@@ -1,8 +0,0 @@
-package twentyJulyTwentyOne.encapsulation.modelss;
-// import twentyJulyTwentyOne.encapsulation.modelss.Students;
-
-public class Teachers {
-    public static void main(String[] args){
-        // System.out.println(Students.getNameMediumPassword());
-    }    
-}
