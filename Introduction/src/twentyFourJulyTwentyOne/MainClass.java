cbd2fbea5a786224507a3e1cb27d943a4d8a9524
Wraient Daemon <81632222+wraient@users.noreply.github.com>
2021-07-25 12:23:16 +0530

    Add files via upload

---
+++ Introduction/src/twentyFourJulyTwentyOne/MainClass.java
@@ -0,0 +1,14 @@
+package twentyFourJulyTwentyOne;
+
+import twentyFourJulyTwentyOne.encaptulation.Student;
+
+public class MainClass {
+    public static void main(String[] args){
+        Student stud1 = new Student("Abhijeet");
+        System.out.println(stud1.getname());
+        System.out.println(stud1.WeakestPass());
+        // System.out.println(stud1.MediumPass()); // Not Visible
+        // System.out.println(stud1.StrongPass()); // Not Visible
+
+    }
+}
